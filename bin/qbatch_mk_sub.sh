05683bc2dd1f5442a7d0664767e11026cc0840ee
nat-tigr <plugkicker@gmail.com>
2018-04-04 17:35:33 -0400

    added abide

--- bin/qbatch_mk_sub.sh
+++ bin/qbatch_mk_sub.sh
@@ -5,14 +5,14 @@ module load python/
 module load R/3.4.3
 module load rstudio/1.1.414
 
-inPath=/projects/edickie/analysis/POND_RST/hcp
+inPath=/projects/edickie/analysis/ABIDEI/hcp/NYU
 cd $inPath
-subjs=`ls -1d MR160*`
+subjs=`ls -1d NYU*`
 
 for subj in $subjs; do
-  echo Rscript temp_stab_glasser.r ${subj}
-done > /scratch/nforde/homotopic/bin/R_qbatch.txt
+  echo Rscript temp_stab_glasser_ABIDE.r ${subj}
+done > /scratch/nforde/homotopic/bin/R_qbatch_abide.txt
 
 cd /scratch/nforde/homotopic/bin/
 
-qbatch --walltime '03:00:00' -b sge --ppj 1 -c 1 -j 1 R_qbatch.txt
+qbatch --walltime '03:00:00' -b sge --ppj 1 -c 1 -j 1 R_qbatch_abide.txt
