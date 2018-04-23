7c941149791f54b3fa07a59a72f7d364d10bd6d2
nat-tigr <plugkicker@gmail.com>
2018-04-23 11:27:30 -0400

    new for diff window size

--- bin/qbatch_mk_sub.sh
+++ bin/qbatch_mk_sub.sh
@@ -5,14 +5,26 @@ module load python/
 module load R/3.4.3
 module load rstudio/1.1.414
 
-inPath=/projects/edickie/analysis/ABIDEI/hcp/NYU
+inPath=/projects/edickie/analysis/POND_RST/hcp
 cd $inPath
-subjs=`ls -1d NYU*`
+subjs=`ls -1d MR160*`
 
 for subj in $subjs; do
-  echo Rscript temp_stab_glasser_ABIDE.r ${subj}
-done > /scratch/nforde/homotopic/bin/R_qbatch_abide.txt
+  echo Rscript temp_stab_glasser.r ${subj}
+done > /scratch/nforde/homotopic/bin/R_qbatch.txt
 
 cd /scratch/nforde/homotopic/bin/
 
-qbatch --walltime '03:00:00' -b sge --ppj 1 -c 1 -j 1 R_qbatch_abide.txt
+qbatch --walltime '10:00:00' -b sge --ppj 1 -c 20 -j 20 R_qbatch.txt
+
+#inPath=/projects/edickie/analysis/ABIDEI/hcp/NYU
+#cd $inPath
+#subjs=`ls -1d NYU*`
+#
+#for subj in $subjs; do
+#  echo Rscript temp_stab_glasser_ABIDE.r ${subj}
+#done > /scratch/nforde/homotopic/bin/R_qbatch_abide.txt
+#
+#cd /scratch/nforde/homotopic/bin/
+#
+#qbatch --walltime '03:00:00' -b sge --ppj 1 -c 1 -j 1 R_qbatch_abide.txt
