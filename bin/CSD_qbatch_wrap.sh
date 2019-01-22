0859fc6c19a4dcb380d1a8f56f66be2439804424
nat-tigr <plugkicker@gmail.com>
2019-01-22 14:55:22 -0500

    changed to slurm queue

--- bin/CSD_qbatch_wrap.sh
+++ bin/CSD_qbatch_wrap.sh
@@ -6,7 +6,7 @@ module load python/3.6_ciftify_01
 cd /KIMEL/tigrlab/scratch/nforde/homotopic/bin
 
 
-for subj in $(<single.txt); do
+for subj in $(<multi.txt); do
 cd /KIMEL/tigrlab/projects/nforde/POND/CSD
 out=/KIMEL/tigrlab/projects/nforde/POND/CSD/$subj
 if [[ ! -d "${out}" ]]; then
@@ -14,33 +14,22 @@ if [[ ! -d "${out}" ]]; then
 fi
 done
 
-for subj in $(<single.txt); do
+cd /KIMEL/tigrlab/scratch/nforde/homotopic/bin
+for subj in $(<multi.txt); do
 out=/KIMEL/tigrlab/projects/nforde/POND/CSD/$subj/prob_DK.csv
 if [[ ! -f "${out}" ]]; then
 	echo $subj
 fi
-done > CSD_single_undone.txt
+done > CSD_multi_undone.txt
 
 
 cd /KIMEL/tigrlab/scratch/nforde/homotopic/bin
-for subj in $(<CSD_single_undone.txt); do
-  echo bash CSD_single.sh $subj
+for subj in $(<CSD_multi_undone.txt); do
+  echo bash CSD_multi.sh $subj
 done > CSD_qbatch.txt
 
 #qbatch  --walltime '48:00:00' -b pbs --ppj 1 -c 1 -j 1 --nodes 1 -o "-l feature=bigmem"  CSD_qbatch.txt
 
 # qbatch --walltime '24:00:00' -b pbs --ppj 20 -c 1 -j 1 --nodes 1 -o "-l mem=130g" CSD_qbatch.txt
 
-qbatch --walltime '10:00:00' -b pbs --ppj 20 -c 5 -j 5 --nodes 1 CSD_qbatch.txt
-
-##-------------------------------------------------------------------------------
-## Probabilistic
-# cd /scratch/nforde/homotopic/POND/CSD/
-# SUBJECTS=`ls -1d MR160*`
-#
-# cd /KIMEL/tigrlab/scratch/nforde/homotopic/bin
-# for subj in $SUBJECTS; do
-#   echo bash CSD_scc_prob.sh $subj
-# done > CSD_prob_batch.txt
-#
-# qbatch --walltime '08:00:00' -b pbs --ppj 20 -c 1 -j 1 --nodes 1 -o "-l mem=130g" CSD_prob_batch.txt
+qbatch --walltime '24:00:00' -b slurm --ppj 20 -c 5 -j 5 --logdir /KIMEL/tigrlab/scratch/nforde/homotopic/bin/CSDlogs CSD_qbatch.txt
